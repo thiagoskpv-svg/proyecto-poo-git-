1c8f825f814f5e68164038be769e5484b669ee59
Santiago Pineda Villarraga <spinedav@udistrital.edu.co>
2026-09-05 20:13:23 -0500

    feat: agrega clase Mago y metodo lanzarHechizo

---
+++ src/gestionproyectos/Mago.java
@@ -0,0 +1,21 @@
+package gestionproyectos;
+
+public class Mago {
+
+    private String nombre;
+    private int mana;
+
+    public Mago(String nombre, int mana) {
+        this.nombre = nombre;
+        this.mana = mana;
+    }
+
+    public boolean lanzarHechizo() {
+        if (mana >= 10) {
+            mana = mana - 10;
+            return true;
+        }
+
+        return false;
+    }
+}
